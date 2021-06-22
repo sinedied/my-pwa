d6008cb12e132bd2a5ebf8a6f30d7b84ae611482
sinedied <noda@free.fr>
2021-06-22 19:22:04 +0200

    add SW updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   template: `
   Message: {{ hello }}
+
+  <p *ngIf="updateAvailable$ | async; else noUpdate">
+  An update is available!
+  Click here to apply:
+  <button (click)="update()">Update</button>
+</p>
+<ng-template #noUpdate>
+  <p>No update available.</p>
+</ng-template>
   `,
   styles: []
 })
 export class AppComponent {
   hello = '';
+  updateAvailable$ = this.swUpdate.available;
+
+  constructor(private swUpdate: SwUpdate) {}
+
+  async update() {
+     await this.swUpdate.activateUpdate();
+     document.location.reload();
+  }
 
   async ngOnInit() {
     try {
